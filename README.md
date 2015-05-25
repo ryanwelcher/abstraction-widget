3b444f1fe9e60dab699ec1be07aaac6c9a9ff1b9
Ryan Welcher <ryan.welcher@10up.com>
2015-05-25 14:45:50 -0400

    Overriding the WP_Widget class and adding new abstraction methods

---
+++ README.md
@@ -0,0 +1,4 @@
+Abstraction Widget Plugin
+==============================
+
+Plugin to test out widget abstraction for [#32470](https://core.trac.wordpress.org/ticket/32470)
---
+++ classes/class-abstracted-widget.php
@@ -0,0 +1,75 @@
+<?php
+
+class My_Widget extends Abstraction_Widget {
+
+	/**
+	 * The Form elements for this widget
+	 * @var array
+	 */
+	public $form_elements = array(
+		array(
+			'type' => 'text',
+			'label'    => 'Name',
+			'name'  => 'name',
+		),
+		array(
+			'type' => 'text',
+			'label' => 'Title',
+			'class' => 'title',
+			'name'  => 'title',
+		),
+		array(
+			'type' => 'checkbox',
+			'label' => 'Remember this status?',
+			'name'  => 'remember',
+		),
+		array(
+			'type' => 'textarea',
+			'label' => 'My Bio',
+			'name' => 'bio',
+		),
+	);
+
+	/**
+	 * Setup the widget
+	 *
+	 * @return array
+	 */
+	function widget_setup() {
+		return array(
+			'id_base'           => 'my-new-widget',
+			'name'              => esc_html__( 'WIDGET!' ),
+			'widget_options'    => array(
+				'classname' => 'my-widget',
+				'description' => esc_html__( 'An Instance my new widget 2' )
+			),
+			'control_options'   => array(),
+		);
+	}
+
+	/**
+	 * Generate the markup for this widget
+	 *
+	 * @param $args
+	 * @param $instance
+	 */
+	function widget_markup( $args, $instance ) {
+		echo '<p>';
+		echo 'The title is : ' . esc_html( $this->widget_title( $instance ) );
+		echo '</p>';
+		echo '<p>';
+		if ( isset(  $instance['remember'] ) ) {
+			echo 'I\'m remembering this setting!';
+		} else {
+			echo 'What setting?';
+		}
+		echo '</p>';
+		echo '<p>';
+		if ( isset( $instance['bio'] ) && ! empty(  $instance['bio'] ) ) {
+			echo $instance['bio'];
+		} else {
+			echo get_bloginfo( 'title' );
+		}
+		echo '</p>';
+	}
+}
